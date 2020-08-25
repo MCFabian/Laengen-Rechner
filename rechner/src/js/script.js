cd092d2e0d4cdd697afb3ceb9c351c2b14385b8e
Fabian <60275181+MCFabian@users.noreply.github.com>
2020-08-25 18:43:28 +0200

    Update script.js

--- rechner/src/js/script.js
+++ rechner/src/js/script.js
@@ -45,6 +45,7 @@ function calculate(){
 	if (fromValue == 1 && toValue == 1) {
 		var ergebnis = sizeValue;
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Zentimeter"; // Ausgabe
 		alert("Fehler");
 	}
 
@@ -53,17 +54,20 @@ function calculate(){
 		checkvalue();
 		ergebnis = ergebnis.replace(".", ",");
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Yard"; // Ausgabe
 
 	}
 
 	else if (fromValue == 1 && toValue == 3) {
 		var ergebnis = "" + (sizeValue / 30.48);
 		document.getElementById("output").value=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Fuß"; // Ausgabe
 	}
 
 	else if (fromValue == 1 && toValue == 4) {
 		var ergebnis = "" + (sizeValue / 2.54);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Zoll"; // Ausgabe
 	}
 
 	// Select Yard 
@@ -71,22 +75,26 @@ function calculate(){
 	else if (fromValue == 2 && toValue == 1) {
 		var ergebnis = "" + (sizeValue * 91.44);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Zentimeter"; // Ausgabe
 	}
 
 	else if (fromValue == 2 && toValue == 2) {
 		var ergebnis = sizeValue;
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Yard"; // Ausgabe
 		alert("Fehler");
 	}
 
 	else if (fromValue == 2 && toValue == 3) {
 		var ergebnis = "" + (sizeValue * 3);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Fuß"; // Ausgabe
 	}
 
 	else if (fromValue == 2 && toValue == 4) {
 		var ergebnis = "" + (sizeValue * 36);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Zoll"; // Ausgabe
 	}
 
 	// Select Fuß
@@ -94,22 +102,26 @@ function calculate(){
 	else if (fromValue == 3 && toValue == 1) {
 		var ergebnis = "" + (sizeValue * 30.48);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Zentimeter"; // Ausgabe
 	}
 
 	else if (fromValue == 3 && toValue == 2) {
 		var ergebnis = "" + (sizeValue / 3);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Yard"; // Ausgabe
 	}
 
 	else if (fromValue == 3 && toValue == 3) {
 		var ergebnis = sizeValue;
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Fuß"; // Ausgabe
 		alert("Fehler");
 	}
 
 	else if (fromValue == 3 && toValue == 4) {
 		var ergebnis = "" + (sizeValue * 12);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Zoll"; // Ausgabe
 	}
 
 	// Select Zoll
@@ -117,21 +129,25 @@ function calculate(){
 	else if (fromValue == 4 && toValue == 1) {
 		var ergebnis = "" + (sizeValue * 2.54);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Zentimeter"; // Ausgabe
 	}
 
 	else if (fromValue == 4 && toValue == 2) {
 		var ergebnis = "" + (sizeValue / 36);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Yard"; // Ausgabe
 	}
 
 	else if (fromValue == 4 && toValue == 3) {
 		var ergebnis = "" + (sizeValue / 12);
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Fuß"; // Ausgabe
 	}
 
 	else if (fromValue == 4 && toValue == 4) {
 		var ergebnis = sizeValue;
 		document.getElementById("output").innerHTML=ergebnis; // Ausgabe
+		document.getElementById("sizer").innerHTML = "Zoll"; // Ausgabe
 		alert("fehler");
 	}
 
